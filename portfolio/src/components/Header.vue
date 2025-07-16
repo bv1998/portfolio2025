f44a55f851f06f4ba843aa30495b71a67658514b
Bryan Vernon <bryan.vernon9@icloud.com>
2025-07-16 09:21:40 -0400

    Init

---
+++ portfolio/src/components/Header.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+    <div id="main-header">
+        <div class="flex flex-row justify-between">
+            <div id="logo">
+                <router-link to="/">
+                    <img src="../assets/logo.png" alt="Logo" />
+                </router-link>
+            </div>
+            <div class="nav"></div>
+            <div class="btn-row flex flex-row gap-6"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#logo img {
+    height: 50px;
+    width: auto;
+}
+</style>
